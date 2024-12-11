795284b5bf58c39366f4ad25e15c22c54339a7c9
Jeff Boggs <jeff_boggs@hotmail.com>
2024-12-10 23:36:06 -0500

    Initial Commit

---
+++ ClearingEnginePOC/ClearingEnginePOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearingEnginePOCApp.swift
+//  ClearingEnginePOC
+//
+//  Created by Jeff Boggs on 2024-12-10.
+//
+
+import SwiftUI
+
+@main
+struct ClearingEnginePOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
